782aa0f4d7a8e76cf496b9caf383bae430d66ccf
Tester_No_test <38783518+A23122005@users.noreply.github.com>
2020-06-06 10:55:36 +0000

    Update bot

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import telebot
-import requests
-bot = telebot.TeleBot('**yor bot key**')
+import argparse
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Найти человека', 'Помощь')
+parser = argparse.ArgumentParser(description='Research vk page.')
+parser.add_argument('key', type=str, help='type a secret telegram bot id, like 1112223334:AAABBBCC4CDDDEEEFFF455GGGH56HHI6IIJ')
+args = parser.parse_args()
+bot_key= args.key
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Доброго времени суток!\nЭтот бот призван найти связи людей.\nДля поиска введите строку типа:\n/research', reply_markup=keyboard1)
-
+bot = telebot.TeleBot(bot_key)
 @bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'Найти человека':
-        bot.send_message(message.chat.id, 'Введите ссылку типа: vk.com/id111222333 или vk.com/durov')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-
-def research(vk_url):
-    pass
-
-bot.polling()
+def get_text_messages(message):
+    splitted_message = message.text.split(' ')
+    if splitted_message[0] == "/start":
+        bot.send_message(message.from_user.id, "Доброго времни суток, вас приветствует бот для помощи в составлении досье на человека. Введте /help для помощи.")
+    elif  splitted_message[0] == "/research":
+        bot.send_message(message.from_user.id, " ".join(splitted_message))
+    elif splitted_message[0] == "/help":
+        bot.send_message(message.from_user.id, "Введите /research vk.com/durov или vk.com/id111222333, для помощи в составлении досье.")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    bot.polling(none_stop=True, interval=0)
+get_text_messages()
---
+++ parser.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Research vk page.')
+parser.add_argument('url', type=str, help='type a vk page, like vk.com/id111222333 or vk.com/durov')
+args = parser.parse_args()
+vk_url = args.url
+
+
+def pase():
+    print(vk_url)
+pase()
