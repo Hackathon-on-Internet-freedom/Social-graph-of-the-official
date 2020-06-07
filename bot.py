e15453478ef0d2a681d36bdf7274de5e258e2ae1
Alexey Frolov <alexfrolov1878@gmail.com>
2020-06-07 01:22:56 -0700

    Use vk id from bot, not from command line.

--- bot.py
+++ bot.py
@@ -11,19 +11,15 @@ _DEFAULT_TOKEN_PATH = 'token_example.txt'
 _DEFAULT_REQ_FIELDS_PATH = 'req_fields.txt'
 
 parser = argparse.ArgumentParser(description='Research vk page.')
-parser.add_argument('--token', type=str, required=True,
+parser.add_argument('--bot_token', type=str, required=True,
                     help=f'telegram bot token (ex. 1112223334:AAABBBCC4CDDDEEEFFF455GGGH56HHI6IIJ)')
-parser.add_argument('--id', type=int, default=_DEFAULT_VK_ID,
-                    help=f'ID of a VK profile (ex. {_DEFAULT_VK_ID})')
 parser.add_argument('--token_path', type=str, default=_DEFAULT_TOKEN_PATH,
                     help='path to a txt file with VK dev token')
 parser.add_argument('--req_fields_path', type=str, default=_DEFAULT_REQ_FIELDS_PATH,
                     help='path to a txt file with the list of profile fields to analyze')
 args = parser.parse_args()
 
-bot = telebot.TeleBot(args.token)
-api = VkApiWrapper(args.id, args.token_path, args.req_fields_path)
-rater = Rater(api)
+bot = telebot.TeleBot(args.bot_token)
 
 
 @bot.message_handler(commands=['start'])
@@ -40,11 +36,14 @@ def find_closest_friends(message):
     vk_id = _DEFAULT_VK_ID
 
     try:
+        api = VkApiWrapper(vk_id, args.token_path, args.req_fields_path)
+        rater = Rater(api)
         friends = rater.get_friends()
         ratings = {}
         for friend in friends['items']:
             ratings[friend] = rater.rate(friend)
-    except:
+    except Exception as e:
+        print(e)
         bot.reply_to(message, 'Что-то пошло не так :(')
         return
 
